de7896d2ba6e7589c862d27f39bbba727a7e7d84
j.sengkeo <j.sengkeo@student.isartdigital.com>
2023-11-01 00:06:44 +0100

    skyrunner project first changelist UE5.2_PS5 first person template
    [git-p4: depot-paths = "//PFE2024_skyrunner/": change = 297784]

---
+++ Source/Skyrunner/SkyrunnerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkyrunnerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASkyrunnerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASkyrunnerGameMode();
+};
+
+
+
