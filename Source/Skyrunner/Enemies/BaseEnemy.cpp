febd8937005b75d1807b8e8889a826da5228c487
a.gerbeaud <a.gerbeaud@student.isartdigital.com>
2023-11-22 12:02:18 +0100

    Addded Gunner enemy, need to fix damage not being taken
    [git-p4: depot-paths = "//PFE2024_skyrunner/": change = 301040]

--- Source/Skyrunner/Enemies/BaseEnemy.cpp
+++ Source/Skyrunner/Enemies/BaseEnemy.cpp
@@ -3,12 +3,30 @@
 
 #include "BaseEnemy.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/CapsuleComponent.h"
+
 // Sets default values
 ABaseEnemy::ABaseEnemy()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	collisionCapsule = CreateDefaultSubobject<UCapsuleComponent>(FName("Collision Capsule"));
+	collisionCapsule->SetCapsuleHalfHeight(95.f);
+	collisionCapsule->SetCapsuleRadius(50.f);
+	RootComponent = collisionCapsule;
+
+	arrowComponent = CreateDefaultSubobject<UArrowComponent>(FName("Arrow Component"));
+	arrowComponent->SetupAttachment(RootComponent);
+
+	detectionSphere = CreateDefaultSubobject<USphereComponent>(FName("Detection Sphere"));
+	detectionSphere->SetSphereRadius(1000.f);
+	detectionSphere->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
--- Source/Skyrunner/Enemies/BaseEnemy.h
+++ Source/Skyrunner/Enemies/BaseEnemy.h
@@ -15,10 +15,21 @@ public:
 	// Sets default values for this pawn's properties
 	ABaseEnemy();
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Components)
+	class UCapsuleComponent* collisionCapsule;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Components)
+	class USphereComponent* detectionSphere;
+
 protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Components)
+	class ACharacter* playerCharacter;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(BlueprintReadOnly, Category = Components)
+	class UArrowComponent* arrowComponent;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
