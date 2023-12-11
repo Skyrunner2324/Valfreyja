66ebc24c109e1271a1acace03731d33df6b86764
j.sengkeo <j.sengkeo@student.isartdigital.com>
2023-12-11 23:02:53 +0100

    automation tool build (does not build from cleaned repo yet)
    [git-p4: depot-paths = "//PFE2024_skyrunner/": change = 305805]

---
+++ BuildCookRunHelp.bat
@@ -0,0 +1,35 @@
+@echo off
+
+echo This script will build the project, build the lightings in Production quality and package the project in Development, Shipping and Test configuration.
+echo.
+echo ctrl+c to stop execution
+echo alt+f4 to stop execution
+echo.
+
+
+
+set UEPATH=
+
+if "%UEPATH%" == "" (
+:: will not process, if the dir was not found, the powershell command will fail
+echo Unreal Engine installation directory was not found please manually enter the directory
+set /p UEPATH="Enter Unreal Engine installation directory>"
+) else (
+echo Unreal Engine installation directory was found
+echo %UEPATH%
+)
+
+
+
+
+:: Unreal Automation Tool
+set UAT="%UEPATH%\Engine\Build\BatchFiles\RunUAT.bat"
+
+
+
+
+
+:: build C++ game code
+call %UAT% BuildCookRun -help
+
+pause
---
+++ Package.bat
@@ -0,0 +1,48 @@
+@echo off
+
+echo This script will build the project, build the lightings in Production quality and package the project in Development, Shipping and Test configuration.
+echo.
+echo ctrl+c to stop execution
+echo alt+f4 to stop execution
+echo.
+
+
+
+set UEPATH=
+
+if "%UEPATH%" == "" (
+:: will not process, if the dir was not found, the powershell command will fail
+echo Unreal Engine installation directory was not found please manually enter the directory
+set /p UEPATH="Enter Unreal Engine installation directory>"
+) else (
+echo Unreal Engine installation directory was found
+echo %UEPATH%
+)
+
+
+
+
+:: Unreal Automation Tool
+set UAT="%UEPATH%\Engine\Build\BatchFiles\RunUAT.bat"
+:: Unreal Engine Editor
+set UEE="%UEPATH%\Engine\Binaries\Win64\UE4Editor-Cmd.exe"
+
+
+
+
+
+:: Unreal .uproject file
+set PROJECT=Skyrunner.uproject
+
+
+
+
+
+
+:: build C++ game code
+call %UAT% BuildGame -project=%~dp0%PROJECT% -platform=Win64+PS5 -clean -build
+
+:: package
+call %UAT% BuildCookRun -project=%~dp0%PROJECT% -platform=Win64+PS5 -CookAll -stage -pak -build -package -configuration=Development+Shipping
+
+pause
