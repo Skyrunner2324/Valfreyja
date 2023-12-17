5e7c6a31aac76c2956a530bbab471e8866edc9cf
j.sengkeo <j.sengkeo@student.isartdigital.com>
2023-12-17 14:17:12 +0100

    updated package.bat
    [git-p4: depot-paths = "//PFE2024_skyrunner/": change = 306451]

--- Package.bat
+++ Package.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo This script will build the project, build the lightings in Production quality and package the project in Development, Shipping and Test configuration.
+echo This script will build the project and package the game for specified configuration and specified platforms.
 echo.
 echo ctrl+c to stop execution
 echo alt+f4 to stop execution
@@ -34,7 +34,10 @@ set UEE="%UEPATH%\Engine\Binaries\Win64\UE4Editor-Cmd.exe"
 :: Unreal .uproject file
 set PROJECT=Skyrunner.uproject
 
+
+
 set /p PLATFORMS="Enter target platforms (Win64+PS5)>"
+set /p CONFIGURATION="Enter package configuration (Development+Test+Shipping)>"
 
 
 
@@ -46,6 +49,6 @@ call %UEE% %~dp0%PROJECT% -run=cook -targetplatform=%PLATFORMS% -cookonthefly -i
 call %UAT% BuildGame -project=%~dp0%PROJECT% -platform=%PLATFORMS% -clean -build
 
 :: package
-call %UAT% BuildCookRun -project=%~dp0%PROJECT% -platform=%PLATFORMS% -CookAll -cook -stage -pak -build -package -configuration=Development+Shipping
+call %UAT% BuildCookRun -project=%~dp0%PROJECT% -platform=%PLATFORMS% -CookAll -cook -stage -pak -build -package -configuration=%CONFIGURATION%
 
 pause
