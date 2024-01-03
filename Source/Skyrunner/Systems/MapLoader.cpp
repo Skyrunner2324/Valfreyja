2060a6e9fbecab3adcb5a5f37f2c5f304bc1fad5
j.sengkeo <j.sengkeo@student.isartdigital.com>
2024-01-03 23:58:20 +0100

    load wip
    [git-p4: depot-paths = "//PFE2024_skyrunner/": change = 308546]

--- Source/Skyrunner/Systems/MapLoader.cpp
+++ Source/Skyrunner/Systems/MapLoader.cpp
@@ -3,6 +3,9 @@
 
 #include "MapLoader.h"
 
+
+#include "Engine/LevelStreamingDynamic.h"
+
 #include "../Utils/DebugLog.h"
 
 
@@ -14,4 +17,22 @@ void UMapLoader::LoadMapAsyncFromName(const FName levelName, FLoadCompleted comp
 void UMapLoader::LoadMapAsync(const TSoftObjectPtr<UWorld> level, FLoadCompleted completed)
 {
 	DebugLogRed(TEXT("Load from soft object reference"));
+
+
+	// TODO : add a delay to test
+	bool success = false;
+	ULevelStreamingDynamic::LoadLevelInstanceBySoftObjectPtr(GEngine->GameViewport->GetWorld(),
+		level,
+		FTransform(),
+		success);
+
+
+	// TODO : find a way of calling completed event when success
+	if (success)
+		DebugLogGreen(TEXT("Successfully loaded"));
+	else
+		DebugLogRed(TEXT("Load failed"));
+
+
+	// TODO : reset (or maybe reset on completed event (user implementation))
 }
